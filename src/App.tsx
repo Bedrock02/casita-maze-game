26e462a2d45c60d97988ffd9ebe0895a1b536884
Bedrock02 <jimsteve91@gmail.com>
2026-03-17 12:14:51 -0400

    Change start button text

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App({ onStart }: AppProps) {
       <img className="casita-image" src={laCasitaFront} alt="La Casita" />
     
       <button className="start-game-btn" type="button" onClick={onStart}>
-        Start Game
+        Empezar
       </button>
     </div>
   );
